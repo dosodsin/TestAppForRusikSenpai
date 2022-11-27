c413d0d9f3363ecfacc26e8f13233a1ede192f55
Ruslan <appaforyou@mail.ru>
2022-11-27 22:55:35 +0300

    Добавил RusikSunpaiApplication

---
+++ app/src/main/java/com/bormotov_vi/RusikSunpaiApplication.kt
@@ -0,0 +1,11 @@
+package com.bormotov_vi
+
+import android.app.Application
+
+class RusikSunpaiApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // проверить что работает и проинициализировать ретрофит
+    }
+}
